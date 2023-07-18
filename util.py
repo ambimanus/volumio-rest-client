e9247bbf30e3933d9e8cd457e481de93f81be9e6
Christian Hinrichs <christian.hinrichs@ambimanus.com>
2023-07-18 21:36:24 +0100

    changed to polling mode instead of push-notifications, now it's working

--- util.py
+++ util.py
@@ -12,14 +12,15 @@ def setup_logger():
     fmt = '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s'
     # https://docs.python.org/3/library/time.html#time.strftime
     datefmt = '%Y-%m-%d %I:%M:%S %z'
-    logging.basicConfig(level=logging.DEBUG, format=fmt, datefmt=datefmt)
+    logging.basicConfig(level=logging.INFO, format=fmt, datefmt=datefmt)
     logger = logging.getLogger('vrc')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     logfile = os.environ['VRC_LOG_PATH']
 
     p_logfile = Path(logfile)
     p_logfile.parent.mkdir(parents=True, exist_ok=True)
     handler = logging.FileHandler(p_logfile)
+    handler.setFormatter(logging.Formatter(fmt=fmt, datefmt=datefmt))
     logger.addHandler(handler)
     return logger
--- vrc.py
+++ vrc.py
@@ -1,56 +1,32 @@
 import datetime
 import dotenv
-import falcon
-import hooks
 import json
 import logging
 import os
 import random
 import requests
+import time
 import util
 
-# https://docs.gunicorn.org/en/latest/run.html#gunicorn
-# run with `gunicorn -c hooks.py -b 0.0.0.0 "vrc:create_app()"`
+
+SLEEP_DURATION_SECONDS = 1
 
 default_library = None
-status_list = []
-current_queue = []
+last_player_state = {'status': '', 'uri': ''}
+last_player_queue = []
 song_history = []
+last_removed_uri = None
 
 program_savestate_filename = None
 
-ignore_events = False
-
 
 def save_program_state():
-    sstate = {'status_list': status_list, 'current_queue': current_queue, 'song_history': song_history}
+    sstate = {'last_player_state': last_player_state,
+              'last_player_queue': last_player_queue,
+              'song_history': song_history}
     global program_savestate_filename
     with open(program_savestate_filename, 'w') as sfile:
         json.dump(sstate, sfile)
-
-
-def evaluate_initial_state():
-    global default_library, current_queue
-    logger = logging.getLogger('vrc')
-
-    state = player_get_state()
-    # Example state: {'status': 'stop', 'position': 0, 'title': '', 'artist': '', 'album': '', 'albumart': '/albumart', 'duration': 0, 'uri': '', 'seek': 0, 'samplerate': '', 'channels': '', 'bitdepth': '', 'Streaming': False, 'service': 'mpd', 'volume': 41, 'dbVolume': None, 'mute': False, 'disableVolumeControl': False, 'random': None, 'repeat': None, 'repeatSingle': False, 'updatedb': False, 'consume': False}
-    logger.debug(f'initial player state: {state}')
-
-    queue = player_get_queue()
-    # Example queue: []
-    if len(current_queue) > 0:
-        logger.error(f'evaluate_initial_state: current_queue not empty: {current_queue}')
-    for song in queue:
-        title = song['title'] if 'title' in song else song['name']
-        s = {'title': title}
-        s['artist'] = song['artist']
-        s['uri'] = song['uri']
-        current_queue.append(s)
-    logger.debug(f'initial player queue: {current_queue}')
-
-    if len(queue) == 0:
-        play_random_song()
 
 
 def play_random_song():
@@ -69,99 +45,8 @@ def get_song_with_index(uri, song_list):
             return song, i
     return None, None
 
-def remove_song_from_both_queues(uri, request_play=True):
-    # remove song from player queue
-    player_remove_uri_from_queue(uri, request_play)
-    # remove song from current_queue storage
-    global current_queue
-    song, index = get_song_with_index(uri, current_queue)
-    if index is not None:
-        del current_queue[index]
 
-
-# https://falcon.readthedocs.io/en/stable/api/hooks.html#after-hooks
-def evaluate_status(req, resp, resource):
-    logger = logging.getLogger('vrc')
-    global status_list, current_queue, song_history
-    if len(status_list) == 0:
-        return
-    media = status_list.pop()
-    logger.debug(f'evaluate_status: {media}')
-    if len(status_list) > 0:
-        logger.warning(f'status_list not empty ({len(status_list)} items remaining)! {status_list}')
-    event = media['item']
-    data = media['data']
-    if event == 'state':
-        if len(data['uri']) != 0:
-            logger.info(f'{data["status"]}: {data["artist"]} - {data["title"]} ({data["uri"]})')
-            if data['status'] == 'play':
-                if len(song_history) == 0 or song_history[-1]['uri'] != data['uri']:
-                        song_history.append({k: data[k] for k in ['artist', 'title', 'uri']})
-                        save_program_state()
-            if data['status'] == 'stop':
-                if 'position' in data:
-                    pos = data['position']
-                    if pos == 0 and len(song_history) > 0:
-                        hist_uri = song_history[-1]['uri']
-                        uri = data['uri']
-                        if uri != hist_uri:
-                            logger.warning(f'uri mismatch: song_history[-1]["uri"] == {hist_uri}')
-                        remove_song_from_both_queues(uri, request_play=False)
-                        save_program_state()
-                    else:
-                        # Special case: A song has finished and another one is waiting in the queue. Volumio then emits
-                        # a 'stop' event for the waiting song (position 1) instead of for the finished one (position 0).
-                        for p in range(pos):
-                            uri = song_history[-1 - p]['uri']
-                            #play = (p == pos - 1)
-                            remove_song_from_both_queues(uri, request_play=False)
-                            save_program_state()
-                else:
-                    hist_uri = song_history[-1]['uri']
-                    uri = data['uri']
-                    if uri != hist_uri:
-                        logger.warning(f'uri mismatch: song_history[-1]["uri"] == {hist_uri}')
-                    remove_song_from_both_queues(uri, request_play=False)
-                    save_program_state()
-                if len(current_queue) == 0:
-                    play_random_song()
-    elif event == 'queue':
-        changed = False
-        queue = []
-        for song in data:
-            title = song['title'] if 'title' in song else song['name']
-            s = {'title': title}
-            s['artist'] = song['artist']
-            s['uri'] = song['uri']
-            queue.append(s)
-        uris_old = {s['uri'] for s in current_queue}
-        uris_new = {s['uri'] for s in queue}
-        for uri in (uris_new - uris_old):
-            song, i = get_song_with_index(uri, queue)
-            if song is None:
-                logger.error(f'uri not found in queue: {uri}')
-            else:
-                changed = True
-                logger.info(f'song added to queue position {i}: {song["artist"]} - {song["title"]} ({song["uri"]})')
-        for uri in (uris_old - uris_new):
-            song, i = get_song_with_index(uri, current_queue)
-            if song is None:
-                logger.error(f'uri not found in current_queue: {uri}')
-            elif len(song_history) == 0 or uri != song_history[-1]['uri']:
-                changed = True
-                logger.info(f'song removed from queue position {i}: {song["artist"]} - {song["title"]} ({song["uri"]})')
-        if changed:
-            current_queue = list(queue)
-            save_program_state()
-            player_state = player_get_state()
-            logger.debug(f'queue changed, this is the current player state: {player_state}')
-            if len(current_queue) == 0 and player_state['status'] == 'stop':
-                play_random_song()
-
-
-# TODO If stop and queue not empty --> request play
-
-def player_remove_uri_from_queue(uri, request_play):
+def player_remove_uri_from_queue(uri):
     logger = logging.getLogger('vrc')
     logger.debug(f'remove_uri_from_queue: {uri}')
 
@@ -169,11 +54,6 @@ def player_remove_uri_from_queue(uri, request_play):
     queue = player_get_queue()
     song, index = get_song_with_index(uri, queue)
     if index is not None:
-        # deactivate the volumio notifications to prevent recursion
-        global ignore_events, current_queue
-        ignore_events = True
-        #hooks.register_with_volumio(register=False)
-
         logger.debug(f'remove song from queue position {index}: {song["artist"]} - {song["name"]} ({song["uri"]})')
         del queue[index]
 
@@ -183,13 +63,6 @@ def player_remove_uri_from_queue(uri, request_play):
         if len(queue) > 0:
             logger.debug(f'populate the queue with the remaining elements: {queue}')
             player_add_to_queue([{"uri": song['uri']} for song in queue])
-            # Start playing the queue
-            if request_play:
-                player_cmd_play()
-
-        # reactivate the volumio notifications
-        #hooks.register_with_volumio()
-        ignore_events = False
 
 
 # https://developers.volumio.com/api/rest-api#music-library
@@ -238,32 +111,94 @@ def player_cmd_play(song_index=0):
     requests.get(req)
 
 
-class VolumioStatus:
+def evaluate_player_state():
+    global last_player_state, last_player_queue, song_history, last_removed_uri
+    logger = logging.getLogger('vrc')
 
-    @falcon.after(evaluate_status)
-    def on_post(self, req, resp):
-        # https://falcon.readthedocs.io/en/stable/api/request_and_response_wsgi.html#falcon.Request.get_media
-        # https://developers.volumio.com/api/rest-api
-        media = req.get_media()
-        logger = logging.getLogger('vrc')
-        global ignore_events
-        if ignore_events:
-            logger.debug(f'[EVENT IGNORED] /volumiostatus --> on_post: {media}')
+    # UPDATE INTERNAL STATE
+
+    player_state = player_get_state()
+    # Example state: {'status': 'stop', 'position': 0, 'title': '', 'artist': '', 'album': '', 'albumart': '/albumart', 'duration': 0, 'uri': '', 'seek': 0, 'samplerate': '', 'channels': '', 'bitdepth': '', 'Streaming': False, 'service': 'mpd', 'volume': 41, 'dbVolume': None, 'mute': False, 'disableVolumeControl': False, 'random': None, 'repeat': None, 'repeatSingle': False, 'updatedb': False, 'consume': False}
+    player_state = {k: player_state[k] for k in ['status', 'title', 'artist', 'uri']}
+    # Shortened state: {'status': 'stop', 'title': '', 'artist': '', 'uri': ''}
+    state_changed = (player_state['status'] != last_player_state['status'] or
+                     player_state['uri'] != last_player_state['uri'])
+    if state_changed:
+        logger.info(f'player state changed: [{player_state["status"]}] {player_state["artist"]} - {player_state["title"]} ({player_state["uri"]})')
+    if player_state['status'] == 'play' and (len(song_history) == 0 or song_history[-1]['uri'] != player_state['uri']):
+        song_history.append(dict(player_state))
+
+    queue_data = player_get_queue()
+    # Example queue: [{'uri': 'spotify:track:5PUvinSo4MNqW7vmomGRS7', 'service': 'spop', 'title': 'Blurred Lines', 'artist': 'Robin Thicke', 'album': 'Blurred Lines', 'type': 'song', 'duration': 263, 'albumart': 'https://i.scdn.co/image/ab67616d0000b2733d74cd7e75846d10c68afd52', 'samplerate': '160 kbps', 'bitdepth': '16 bit', 'bitrate': '', 'trackType': 'spotify'}]
+    queue_changed = False
+    player_queue = []
+    for song in queue_data:
+        title = song['title'] if 'title' in song else song['name']
+        s = {'title': title}
+        s['artist'] = song['artist']
+        s['uri'] = song['uri']
+        player_queue.append(s)
+    # Shortened queue: [{'uri': 'spotify:track:5PUvinSo4MNqW7vmomGRS7', 'title': 'Blurred Lines', 'artist': 'Robin Thicke'}]
+    uris_old = {s['uri'] for s in last_player_queue}
+    uris_new = {s['uri'] for s in player_queue}
+    # Loop over removed songs
+    for uri in (uris_old - uris_new):
+        song, i = get_song_with_index(uri, last_player_queue)
+        if song is None:
+            logger.error(f'uri not found in last_player_queue: {uri}')
         else:
-            logger.debug(f'/volumiostatus --> on_post: {media}')
-            global status_list
-            status_list.append(media)
-        resp.status = falcon.HTTP_200
+            queue_changed = True
+            logger.info(f'song removed from queue position {i}: {song["artist"]} - {song["title"]} ({song["uri"]})')
+    # Loop over added songs
+    for uri in (uris_new - uris_old):
+        song, i = get_song_with_index(uri, player_queue)
+        if song is None:
+            logger.error(f'uri not found in player_queue: {uri}')
+        else:
+            queue_changed = True
+            logger.info(f'song added to queue position {i}: {song["artist"]} - {song["title"]} ({song["uri"]})')
+
+    # Store state to disk if something changed
+    if state_changed or queue_changed:
+        last_player_state = player_state
+        last_player_queue = player_queue
+        logger.debug(f'state or queue changed, saving program state')
+        save_program_state()
 
 
-def create_app():
+    # TAKE ACTIONS
+    # (will be reported in next loop)
+
+    # Housekeeping: Remove last played song from first queue position
+    if len(song_history) > 0:
+        uri = song_history[-1]['uri']
+        if last_player_state['status'] == 'stop' and len(last_player_queue) > 0:
+            if uri == last_player_queue[0]['uri']:
+                player_remove_uri_from_queue(uri)
+                last_removed_uri = uri
+        if last_player_state['status'] == 'play' and len(last_player_queue) > 1:
+            if uri == last_player_queue[1]['uri']:
+                player_remove_uri_from_queue(uri)
+                last_removed_uri = uri
+
+    # Check if the player is idle and if we should play a song
+    if (not state_changed) and (not queue_changed) and (player_get_state()['status'] == 'stop'):
+        # Play random song if queue is empty or if it only contains the previously played song
+        if (len(last_player_queue) == 0) or (len(last_player_queue) == 1 and last_player_queue[0]['uri'] == last_removed_uri):
+            play_random_song()
+        # otherwise, play next song if queue is not empty
+        elif len(last_player_queue) > 0:
+            player_cmd_play()
+
+
+def main():
     dotenv.load_dotenv()
     util.setup_logger()
     logger = logging.getLogger('vrc')
     logger.debug('vrc starting')
 
-    #qfilename = './queue.json'
-    qfilename = './queue_short.json'
+    qfilename = './queue.json'
+    #qfilename = './queue_short.json'
     with open(qfilename, 'r') as queuefile:
         queue = json.load(queuefile)['queue']
         global default_library
@@ -274,25 +209,11 @@ def create_app():
         } for song in queue]
     global program_savestate_filename
     program_savestate_filename = f'{datetime.datetime.now().isoformat().replace(":", "-")}.savestate.json'
-    evaluate_initial_state()
 
-    app = falcon.App()
-    app.add_route('/volumiostatus', VolumioStatus())
-    return app
+    while True:
+        evaluate_player_state()
+        time.sleep(SLEEP_DURATION_SECONDS)
 
 
 if __name__ == '__main__':
-
-    class Mock:
-        def get_media(self):
-            queue = """
-            {"queue": [{"uri": "spotify:track:4eRmCZWJoEtLl0wy7EJPwd", "service": "spop", "name": "Dickes B (feat. Black Kappa)", "artist": "Seeed", "album": "New Dubby Conquerors", "type": "song", "duration": 284, "albumart": "https://i.scdn.co/image/ab67616d0000b27389917cfa3391d5c872396e6f", "samplerate": "160 kbps", "bitdepth": "16 bit", "bitrate": "", "trackType": "spotify"}, {"uri": "spotify:track:6b2vkaiDawOcupyDxNNCba", "service": "spop", "name": "Dancehall Caballeros", "artist": "Seeed", "album": "New Dubby Conquerors", "type": "song", "duration": 192, "albumart": "https://i.scdn.co/image/ab67616d0000b27389917cfa3391d5c872396e6f", "samplerate": "160 kbps", "bitdepth": "16 bit", "bitrate": "", "trackType": "spotify"}, {"uri": "spotify:track:7A4KdLy1DXOOC5fhIdDuHz", "service": "spop", "name": "Haus am See", "artist": "Peter Fox", "album": "Stadtaffe", "type": "song", "duration": 216, "albumart": "https://i.scdn.co/image/ab67616d0000b2739061a4e47413aff8f58a3c9c", "samplerate": "160 kbps", "bitdepth": "16 bit", "bitrate": "", "trackType": "spotify"}, {"uri": "spotify:track:1ooatRQWNtl4jB7hQoanFz", "service": "spop", "name": "Riddim No 1", "artist": "Seeed", "album": "New Dubby Conquerors", "type": "song", "duration": 233, "albumart": "https://i.scdn.co/image/ab67616d0000b27389917cfa3391d5c872396e6f", "samplerate": "160 kbps", "bitdepth": "16 bit", "bitrate": "", "trackType": "spotify"}, {"uri": "spotify:track:0c4IEciLCDdXEhhKxj4ThA", "service": "spop", "name": "Madness", "artist": "Muse", "album": "The 2nd Law", "type": "song", "duration": 281, "albumart": "https://i.scdn.co/image/ab67616d0000b273fc192c54d1823a04ffb6c8c9", "samplerate": "160 kbps", "bitdepth": "16 bit", "bitrate": "", "trackType": "spotify"}]}
-            """
-            state = """
-            {"item": "state", "data": {"status": "play", "title": "Dancehall Caballeros", "artist": "Seeed", "album": "New Dubby Conquerors", "albumart": "https://i.scdn.co/image/ab67616d0000b27389917cfa3391d5c872396e6f", "uri": "spotify:track:6b2vkaiDawOcupyDxNNCba", "trackType": "spotify", "seek": 187683, "duration": 193, "samplerate": "160 kbps", "bitdepth": "16 bit", "channels": 2, "consume": false, "volume": 8, "dbVolume": "", "mute": false, "disableVolumeControl": false, "stream": false, "updatedb": false, "volatile": true, "service": "spop"}}
-            """
-            return json.loads(state)
-
-    dotenv.load_dotenv()
-    # test update_queue
-    update_queue(Mock(), None, None, None)
+    main()
